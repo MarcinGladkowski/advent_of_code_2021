c1bb1e9b790ce3e7713457db2d7627e54a91b988
Marcin Gladkowski <marcingladkowski@gmail.com>
2021-12-04 21:00:01 +0100

    day_4: working on main loop

--- day_4/main.py
+++ day_4/main.py
@@ -1,2 +1,16 @@
 with open('test_input.txt') as f:
     data = f.read().split('\n')
+
+    numbers = data[0]
+
+    bingo_boards = data[1:]
+    # filter empty lines
+
+
+    for bingo in bingo_boards:
+        if bingo != '':
+            rows = bingo.split('\n')
+
+            for row in rows:
+                print(row)
+
