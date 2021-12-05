8419754e3adea1caa3260643dca4e38dac4598a5
Marcin Gladkowski <marcingladkowski@gmail.com>
2021-12-05 12:53:06 +0100

    day_4: find winning row with numbers set

--- day_4/main.py
+++ day_4/main.py
@@ -1,16 +1,37 @@
+def play(numbers_set, boards):
+    for board in boards:
+        if board != '':
+            rows = board.split('\n')
+
+            for row in rows:
+                row = [int(r) for r in row.split(' ') if r != '']
+
+                print(row, numbers_set)
+
+                if set(row).issubset(set(numbers_set)):
+                    print('The winner set is ', row)
+
+                    return True
+    return False
+
+
 with open('test_input.txt') as f:
     data = f.read().split('\n')
 
-    numbers = data[0]
+    numbers = [int(n) for n in data[0].split(',')]
 
     bingo_boards = data[1:]
+    initial_offset = 5
     # filter empty lines
+    numbers_play_set = numbers[0:initial_offset]
 
+    index = 0
+    while True:
+        index += 1
 
-    for bingo in bingo_boards:
-        if bingo != '':
-            rows = bingo.split('\n')
+        if play(numbers[0:initial_offset+index], bingo_boards):
+            print('Winner!')
+            break
 
-            for row in rows:
-                print(row)
-
+        if initial_offset+index >= len(numbers):
+            raise print('Finish lottery!')
