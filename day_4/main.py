a8d52bd2143cc223537c2320b5fbc640a4ccfba3
Marcin Gladkowski <marcingladkowski@gmail.com>
2021-12-05 14:07:08 +0100

    day_4: prepare chunks of data

--- day_4/main.py
+++ day_4/main.py
@@ -1,3 +1,6 @@
+def calculate_win(lottery_numbers, bingo_board):
+    pass
+
 def play(numbers_set, boards):
     for board in boards:
         if board != '':
@@ -5,12 +8,9 @@ def play(numbers_set, boards):
 
             for row in rows:
                 row = [int(r) for r in row.split(' ') if r != '']
-
-                print(row, numbers_set)
-
                 if set(row).issubset(set(numbers_set)):
                     print('The winner set is ', row)
-
+                    # get last win number and get sum of unmarked
                     return True
     return False
 
@@ -21,17 +21,21 @@ with open('test_input.txt') as f:
     numbers = [int(n) for n in data[0].split(',')]
 
     bingo_boards = data[1:]
+
+    parsed = [board for i, board in enumerate(bingo_boards) if board != '']
+    chunks = [parsed[i:i+5] for i in range(0, len(bingo_boards), 5) if bingo_boards != '']
+
     initial_offset = 5
     # filter empty lines
     numbers_play_set = numbers[0:initial_offset]
 
-    index = 0
+    bid = 0
     while True:
-        index += 1
+        bid += 1
 
-        if play(numbers[0:initial_offset+index], bingo_boards):
+        if play(numbers[0:initial_offset+bid], bingo_boards):
             print('Winner!')
             break
 
-        if initial_offset+index >= len(numbers):
+        if initial_offset+bid >= len(numbers):
             raise print('Finish lottery!')
