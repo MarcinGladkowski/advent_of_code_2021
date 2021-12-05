1e2977a4e6e58f866da47a4c6a7d81c963e5d978
Marcin Gladkowski <marcingladkowski@gmail.com>
2021-12-05 14:45:55 +0100

    day_4: function to calculate sum of unmarked elements

--- day_4/main.py
+++ day_4/main.py
@@ -1,18 +1,52 @@
-def calculate_win(lottery_numbers, bingo_board):
-    pass
+def calculate_unmarked(lottery_numbers, board):
+    diff = []
+
+    for row in board:
+        row_diff = list(set(row) - set(lottery_numbers))
+        for d in row_diff:
+            diff.append(d)
+
+    return sum(diff)
+
+
+win_number_set = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24]
+unmarked_board = [
+    [14, 21, 17, 24, 4],
+    [10, 16, 15, 9, 19],
+    [18, 8, 23, 26, 20],
+    [22, 11, 13, 6, 5],
+    [2, 0, 12, 3, 7]
+]
+
+assert 188 == calculate_unmarked(win_number_set, unmarked_board)
+
 
 def play(numbers_set, boards):
+    diff = []
+    won = False
+    print("Start lotterry!")
     for board in boards:
-        if board != '':
-            rows = board.split('\n')
 
-            for row in rows:
-                row = [int(r) for r in row.split(' ') if r != '']
-                if set(row).issubset(set(numbers_set)):
-                    print('The winner set is ', row)
-                    # get last win number and get sum of unmarked
-                    return True
-    return False
+        print(board)
+
+        for row in board:
+            # diff of numbers from row and lottery
+            row = [int(r) for r in row.split(' ') if r != '']  # parsing each row
+            print(row)
+
+            row_diff = list(set(row) - set(numbers_set))
+            for d in row_diff:
+                diff.append(d)
+
+            if set(row).issubset(set(numbers_set)):
+                won = True
+
+
+
+    print(sum(diff))
+    print(diff)
+
+    return won
 
 
 with open('test_input.txt') as f:
@@ -23,7 +57,7 @@ with open('test_input.txt') as f:
     bingo_boards = data[1:]
 
     parsed = [board for i, board in enumerate(bingo_boards) if board != '']
-    chunks = [parsed[i:i+5] for i in range(0, len(bingo_boards), 5) if bingo_boards != '']
+    chunks = [parsed[i:i + 5] for i in range(0, len(bingo_boards), 5) if bingo_boards != '']
 
     initial_offset = 5
     # filter empty lines
@@ -33,9 +67,9 @@ with open('test_input.txt') as f:
     while True:
         bid += 1
 
-        if play(numbers[0:initial_offset+bid], bingo_boards):
+        if play(numbers[0:initial_offset + bid], chunks):
             print('Winner!')
             break
 
-        if initial_offset+bid >= len(numbers):
+        if initial_offset + bid >= len(numbers):
             raise print('Finish lottery!')
